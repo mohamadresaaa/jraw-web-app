3e67e8fb6d4ef8b5b159f60d1b08e4e4bfe1f373
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-01-01 23:00:42 +0330

    reset password page

--- src/views/auth/ResetPassword.vue
+++ src/views/auth/ResetPassword.vue
@@ -1,8 +1,8 @@
 <template>
   <v-container fluid fill-height>
     <v-layout align-center justify-center>
-      <v-flex xs12 sm8 md4>
-        <v-card outlined>
+      <v-flex xs12 sm6 md3>
+        <v-card v-if="!this.$route.params.code" outlined>
           <v-card-text>
             <!-- title -->
             <h1 class="text-center text-center mt-5">Reset your password</h1>
@@ -10,23 +10,49 @@
           <!-- forgot password form -->
           <v-form v-model="isFormValid" lazy-validation ref="form" @submit.prevent>
             <v-card-text>
-              <v-alert
-                text
-                color="orange darken-4"
-              >Enter your user account's verified email address and we will send you a password reset link</v-alert>
+              <v-alert class="subtitle-2" color="orange darken-4" text>Enter your user account's verified email address
+                and we will
+                send you a password reset link</v-alert>
               <!-- email field -->
               <v-text-field v-model="email" :rules="emailRules" label="Email" autofocus dense outlined></v-text-field>
             </v-card-text>
             <v-card-actions class="mx-5">
               <v-spacer></v-spacer>
               <!-- set loading -->
-              <v-btn
-                :disabled="!isFormValid"
-                color="green"
-                class="text-capitalize white--text mb-5"
-                type="submit"
-                block
-              >Send password reset email</v-btn>
+              <v-btn :disabled="!isFormValid" color="green" class="text-capitalize white--text mb-5" type="submit"
+                block>Send password reset email</v-btn>
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-form>
+        </v-card>
+
+        <v-card v-else outlined>
+          <v-card-text class="text-center text-center">
+            <v-avatar color="blue accent-4" size="100">
+              <img src="https://avatars2.githubusercontent.com/u/41260098?s=400&v=4" alt="mohamadresaaa">
+              <!-- <span class="white--text headline">80</span> -->
+            </v-avatar>
+            <v-spacer></v-spacer>
+            <v-chip class="mt-5 subtitle-2" text-color="blue accent-4" label pill outlined>
+              mohamadresaaa
+            </v-chip>
+          </v-card-text>
+          <!-- reset password form -->
+          <v-form v-model="isFormValid" lazy-validation ref="form" @submit.prevent>
+            <v-card-text>
+              <!-- new password field -->
+              <v-text-field v-model="password" :rules="passwordRules" label="New password"
+                :type="showPassword ? 'text' : 'password'" :append-icon="showPassword ? 'visibility' : 'visibility_off'"
+                @click:append="showPassword = !showPassword" autofocus dense outlined></v-text-field>
+
+              <!-- confirm password field -->
+              <v-text-field v-model="confirmPassword" :rules="confirmPasswordRules" label="Confirm password"
+                type="password" dense outlined></v-text-field>
+            </v-card-text>
+            <v-card-actions class="mx-5">
+              <v-spacer></v-spacer>
+              <v-btn :disabled="!isFormValid" color="green" class="text-capitalize white--text mb-5" type="submit"
+                block>reset password</v-btn>
               <v-spacer></v-spacer>
             </v-card-actions>
           </v-form>
@@ -51,4 +77,5 @@ export default {
     }
   }
 }
+
 </script>
