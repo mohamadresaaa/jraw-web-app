b68cde8d1cd7b461a75d69b74e8b56a6ebc98e7a
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-01-07 11:59:45 +0330

    update change password

--- src/views/user/ChangePassword.vue
+++ src/views/user/ChangePassword.vue
@@ -1,41 +1,37 @@
 <template>
-    <v-card>
-        <v-container>
-            <v-layout wrap>
-                <!-- title -->
-                <v-flex xs12 sm12 md12>
-                    <v-card-title>
-                        <h3 class="font-weight-medium text-capitalize">Change password</h3>
-                    </v-card-title>
-                </v-flex>
-                <v-flex xs12 sm12 md12>
-                    <!-- change password form -->
-                    <v-form v-model="isFormValid" lazy-validation ref="form" @submit.prevent="">
-                        <!-- old password field -->
-                        <v-text-field v-model="oldPassword" :rules="oldPasswordRules" label="Old password"
-                            :type="showOldPassword ? 'text' : 'password'"
-                            :append-icon="showOldPassword ? 'visibility' : 'visibility_off'"
-                            @click:append="showOldPassword = !showOldPassword" outlined
-                dense></v-text-field>
-
-                        <!-- new password field -->
-                        <v-text-field v-model="newPassword" :rules="newPasswordRules" label="New password"
-                            :type="showNewPassword ? 'text' : 'password'"
-                            :append-icon="showNewPassword ? 'visibility' : 'visibility_off'"
-                            @click:append="showNewPassword = !showNewPassword" outlined
-                dense></v-text-field>
-
-                        <!-- confirm new password field -->
-                        <v-text-field v-model="newConfirmPassword" :rules="newConfirmPasswordRules"
-                            label="Confirm new password" type="password" outlined dense></v-text-field>
-                        <v-btn color="blue darken-1" class="ml-0 text-capitalize white--text" type="submit" outlined>
-                            Change password
-                        </v-btn>
-                    </v-form>
-                </v-flex>
-            </v-layout>
-        </v-container>
-    </v-card>
+  <v-card>
+    <v-container>
+      <v-layout wrap>
+        <!-- title -->
+        <v-flex xs12 sm12 md12>
+          <v-card-title>
+            <h3 class="font-weight-medium text-capitalize">Change password</h3>
+          </v-card-title>
+        </v-flex>
+        <v-flex xs12 sm12 md12>
+          <!-- change password form -->
+          <v-form v-model="isFormValid" lazy-validation ref="form" @submit.prevent="">
+            <!-- old password field -->
+            <v-text-field v-model="oldPassword" :rules="oldPasswordRules" label="Old password"
+              :type="showOldPassword ? 'text' : 'password'"
+              :append-icon="showOldPassword ? 'visibility' : 'visibility_off'"
+              @click:append="showOldPassword = !showOldPassword" outlined dense></v-text-field>
+            <!-- new password field -->
+            <v-text-field v-model="newPassword" :rules="newPasswordRules" label="New password"
+              :type="showNewPassword ? 'text' : 'password'"
+              :append-icon="showNewPassword ? 'visibility' : 'visibility_off'"
+              @click:append="showNewPassword = !showNewPassword" outlined dense></v-text-field>
+            <!-- confirm new password field -->
+            <v-text-field v-model="newConfirmPassword" :rules="newConfirmPasswordRules" label="Confirm new password"
+              type="password" outlined dense></v-text-field>
+            <v-btn color="blue darken-1" class="ml-0 text-capitalize white--text" type="submit" outlined>
+              Change password
+            </v-btn>
+          </v-form>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-card>
 </template>
 
 <script>
@@ -56,7 +52,7 @@ export default {
         newPassword => newPassword.length >= 8 || "New password must be at least 8 characters"
       ],
       newConfirmPasswordRules: [newConfirmPassword => newConfirmPassword === this.newPassword ||
-                    "Passwords must match"
+          "Passwords must match"
       ]
     }
   },
@@ -64,4 +60,5 @@ export default {
 
   }
 }
+
 </script>
