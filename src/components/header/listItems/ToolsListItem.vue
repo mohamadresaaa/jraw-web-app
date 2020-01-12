5e4050e00f11dc1a59dd18019ebc36ee1b4e7f28
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-01-12 12:57:59 +0330

    update user list item

--- src/components/header/listItems/ToolsListItem.vue
+++ src/components/header/listItems/ToolsListItem.vue
@@ -24,7 +24,7 @@ export default {
       },
       {
         text: "Contacts",
-        icon: "people",
+        icon: "contacts",
         path: "contacts"
       },
       {
--- src/components/header/listItems/UserListItem.vue
+++ src/components/header/listItems/UserListItem.vue
@@ -1,37 +1,59 @@
 <template>
-  <v-list-group prepend-icon="settings">
-    <!-- title -->
-    <template v-slot:activator>
-      <v-list-item>
-        <v-list-item-title>Settings</v-list-item-title>
+  <div>
+    <v-list-group prepend-icon="security">
+      <!-- title -->
+      <template v-slot:activator>
+        <v-list-item>
+          <v-list-item-title>Security</v-list-item-title>
+        </v-list-item>
+      </template>
+      <!-- items -->
+      <v-list-item v-for="(item, index) in security_items" :key="index" :to="item.path">
+        <v-list-item-action>
+          <v-icon v-text="item.icon"></v-icon>
+        </v-list-item-action>
+        <v-list-item-title v-text="item.text"></v-list-item-title>
       </v-list-item>
-    </template>
-    <!-- items -->
-    <v-list-item v-for="(item, index) in items" :key="index" :to="item.path">
-      <v-list-item-action>
-        <v-icon v-text="item.icon"></v-icon>
-      </v-list-item-action>
-      <v-list-item-title v-text="item.text"></v-list-item-title>
-    </v-list-item>
-  </v-list-group>
+    </v-list-group>
+    <v-list-group prepend-icon="settings">
+      <!-- title -->
+      <template v-slot:activator>
+        <v-list-item>
+          <v-list-item-title>Settings</v-list-item-title>
+        </v-list-item>
+      </template>
+      <!-- items -->
+      <v-list-item v-for="(item, index) in setting_items" :key="index" :to="item.path">
+        <v-list-item-action>
+          <v-icon v-text="item.icon"></v-icon>
+        </v-list-item-action>
+        <v-list-item-title v-text="item.text"></v-list-item-title>
+      </v-list-item>
+    </v-list-group>
+  </div>
 </template>
 
 <script>
 export default {
   data () {
     return {
-      items: [{
+      setting_items: [{
         text: "Edit Profile",
         path: "/user/edit_profile"
       },
       {
         text: "Privacy",
         path: "/user/account"
-      },
-      {
-        text: "Change Password",
-        path: "/user/change_password"
-      }
+      }],
+      security_items: [
+        {
+          text: "Change password",
+          path: "/user/change_password"
+        },
+        {
+          text: "sessions",
+          path: "/user/sessions"
+        }
       ]
     }
   }
