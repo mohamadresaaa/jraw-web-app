6bfaf5b311f0a37075f2bfe95e1feb1738070059
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-01-19 10:39:18 +0330

    create axios and set loading

---
+++ src/lib/http.js
@@ -0,0 +1,42 @@
+import axios from "axios"
+import store from "@/store"
+
+// Set url and headers
+const http = axios.create({
+  baseURL: "http://localhost:300",
+  headers: {
+    "authorization": `Bearer ${localStorage.getItem("access-jraw")}`
+  }
+})
+
+// Enabled loading
+http.interceptors.request.use(config => {
+  store.commit("setMainState", {
+    resource: "loading",
+    item: true
+  })
+  return config
+}, error => {
+  store.commit("setMainState", {
+    resource: "loading",
+    item: false
+  })
+  return Promise.reject(error)
+})
+
+// Disabled loading
+http.interceptors.response.use(config => {
+  store.commit("setMainState", {
+    resource: "loading",
+    item: false
+  })
+  return config
+}, error => {
+  store.commit("setMainState", {
+    resource: "loading",
+    item: false
+  })
+  return Promise.reject(error)
+})
+
+export default http
