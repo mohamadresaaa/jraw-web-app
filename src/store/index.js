3a6a400b5cedf27e476e807b76a91237915e9b16
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-01-19 10:43:15 +0330

    define loading and message state

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,28 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    loading: false,
+    message: null
+  },
+  getters: {
+    loading: state => state.loading,
+    message: state => state.message
   },
   mutations: {
+    setItems (state, { resource, items }) {
+      state[resource].items = items
+    },
+    setItem (state, { resource, item }) {
+      state[resource].item = item
+    },
+    setMainState (state, { resource, item }) {
+      state[resource] = item
+    }
   },
   actions: {
+    clearMessage ({ commit }) {
+      return commit("setMainState", { resource: "message", item: null })
+    }
   },
   modules: {
   }
