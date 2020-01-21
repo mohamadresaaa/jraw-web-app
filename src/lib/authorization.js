80fbe4494507837bc93e62245d6cc4f748da3308
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-01-21 12:13:03 +0330

    add getUser

--- src/lib/authorization.js
+++ src/lib/authorization.js
@@ -1,5 +1,27 @@
 import store from "@/store"
 
+// get user authenticated
+export const getUser = (to, from, next) => {
+  store.dispatch("auth/getAuthUser")
+    .then(user => {
+      // only users who have been authenticated
+      if (to.meta.onlyUsers) {
+        if (user) {
+          next()
+        } else {
+          next({
+            name: "home"
+          })
+        }
+      } else {
+        next()
+      }
+    })
+    .catch(() => {
+      next({ name: "home" })
+    })
+}
+
 // If the user is authenticated it should not see authentication pages
 export const isAuthenticated = (to, from, next) => {
   store.getters["auth/isAuthenticated"] ? next({ name: "home" }) : next()
