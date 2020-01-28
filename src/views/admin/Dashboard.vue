e427313941468b75fffc3114ef1a7ff8c8597842
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-01-28 22:17:07 +0330

    add dashboard page

---
+++ src/views/admin/Dashboard.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-container>
+    <v-layout wrap>
+      <v-flex xs12 sm12 md12>
+        <v-card-title class="px-0 mx-0 mb-4">
+          <h3 class="font-weight-medium text-capitalize">Dashboard</h3>
+        </v-card-title>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
