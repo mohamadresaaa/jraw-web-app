fa295a2467a65e079f7890e4f283103f8ecf464e
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-01-28 19:06:29 +0330

    add doughnut chart in about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <v-card>
+      <doughnutChart />
+    </v-card>
+
   </div>
 </template>
+
+<script>
+import doughnutChart from "../components/charts/doughnutChart"
+
+export default {
+  components: {
+    doughnutChart
+  }
+}
+</script>
