47e4b1cf4f7e85b56f54409f45a30783e1ee396d
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-03-05 12:16:36 +0330

    fix about page

--- src/router/index.js
+++ src/router/index.js
@@ -20,11 +20,11 @@ const routes = [
       {
         path: "about",
         name: "about",
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () =>
-          import(/* webpackChunkName: "about" */ "../views/About.vue")
+        component: StatusPage,
+        props: {
+          title: "Oops!",
+          message: "We can't find the page you're looking for.Try searching our posts"
+        }
       },
       {
         path: "contact",
